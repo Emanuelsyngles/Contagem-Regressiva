d6cfdbd202a4ad7c48ea9af1554c14252428a89f
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-09 13:45:55 -0300

    ...

---
+++ contagem/src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Counter = (title, number) => {
+  return (
+    <div>
+      <div className='counter'>
+        <p className="counter-number"></p>
+        <h3 className="counter-text"></h3>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
