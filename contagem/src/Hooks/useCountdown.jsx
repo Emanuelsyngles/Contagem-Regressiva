fb2511ebcaa7fd91da2a77f2884f145d69786a06
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-12 20:51:08 -0300

    Update useCountdown.jsx

--- contagem/src/Hooks/useCountdown.jsx
+++ contagem/src/Hooks/useCountdown.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 
 const useCountdown = (date) => {
-    const countDate = new Date(date).getTime();
-    const now = new Date().getTime();
 
-    const interval = countDate - now;
+    const [day, setDay] = useState()
+    const [hour, setHour] = useState()
+    const [minute, setMinutes] = useState()
+    const [second, setSecond] = useState()
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    const dayNumber = Math.floor(interval / day);
-    const hourNumber = Math.floor((interval % day) / hour);
-    const minuteNumber = Math.floor((interval % hour) / minute);
-    const secondNumber = Math.floor((interval % minute) / second);
-
+    const countdown = () => {
+        const countDate = new Date(date).getTime();
+        const now = new Date().getTime();
     
-    console.log(dayNumber, hourNumber, minute, secondNumber)
+        const interval = countDate - now;
+    
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+    
+        const dayNumber = Math.floor(interval / day);
+        const hourNumber = Math.floor((interval % day) / hour);
+        const minuteNumber = Math.floor((interval % hour) / minute);
+        const secondNumber = Math.floor((interval % minute) / second);
+    }
+    
+    setInterval(countdown, 1000);
 
     return 1;
 };
